fae6bfb591d3e0861171363f888097c42f485c37
anig1scur <anigiscur@gmail.com>
2025-01-24 10:56:31 -0600

    chore: gh action deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+import adapterStatic from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -11,7 +12,18 @@ const config = {
     // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
-    adapter: adapter()
+    adapter: adapterStatic(
+      {
+        pages: 'build',
+        assets: 'build',
+        fallback: null,
+        precompress: false,
+        strict: true
+      }
+    ),
+    paths: {
+      base: '',
+    },
   },
   onwarn: (warning, handler) => {
     const { code } = warning;
