87eb695bad60731d0d93e6101d54fade18dbff35
anig1scur <anigiscur@gmail.com>
2026-01-01 11:38:20 -0600

    update font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,22 @@
 import typography from '@tailwindcss/typography';
-import type { Config } from 'tailwindcss';
+import type {Config} from 'tailwindcss';
 
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {
-      
+      fontFamily: {
+        sans:
+            [
+              'ui-monospace', 'SFMono-Regular', 'Menlo', 'Monaco', 'Consolas',
+              '"Liberation Mono"', '"Courier New"',
+
+              '"Microsoft YaHei"', '"PingFang SC"', '"Hiragino Sans GB"',
+              '"Heiti SC"', '"WenQuanYi Micro Hei"',
+
+              'sans-serif'
+            ],
+      },
     }
   },
 
